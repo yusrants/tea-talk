080ddddc316770c1ce518264dcf751d087c4ec9e
yusrants <yusrants@users.noreply.github.com>
2022-06-09 18:49:53 +0500

    Add sending messages

--- server/index.js
+++ server/index.js
@@ -19,11 +19,32 @@ io.on('connection', (socket) => {
         console.log('user has left')
     });
 
+    // Connect and add a user to the room
     socket.on('join', ({ name, room},callback)=>{
-        console.log(name, room);
-    
+
+        const { user, error} = addUser({id: socket.id, name, room});
+
+        if (error) return callback(error);
+
+        socket.emit('message', {user: 'admin', text: `Hi ${user.name}! Welcome to the room ${user.room}`});
+
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `Attention everyone! ${user.name} has joined.`});
+
+        socket.join(user.room);
+
+        callback;
     });
+
+    socket.on('sendMessage',(message, callback)=> {
+        const user = getUser(socket.id);
+
+        //Send user message to the room
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+    })
 });
+
+    
 
 app.use(router);
 
